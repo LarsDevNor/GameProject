2c153762283c5dd7e44839a50d022ab6922146ee
AndreasDev <Andreas@Andreas-PC.(none)>
2011-12-03 03:28:14 -0800

    Camera and input

--- OpenGLProject/Camera.h
+++ OpenGLProject/Camera.h
@@ -14,39 +14,30 @@ public:
 	virtual ~Camera(void);
 
 	void init();
-	void lookAt();
-	void lookAt(const glm::vec3 &eye, const glm::vec3 &target, const glm::vec3 &up);
 	void setProjection();
-	void setTarget(const glm::vec3& target);
-	void rotate(float deg, const glm::vec3& axis);
-	
-	void moveLeft(float dirAndStrength);
-	//void moveRight();
+
+	void rotateX(float deg);
+	void rotateY(float deg);
 	void moveForward(float dirAndStrength);
-	//void moveBackward();
-	void zoom(int amount);
-	void rotateView(float dirAndStrength);
 
 	glm::vec3 getEye() const { return eye; }
-	glm::vec3 getTarget() const { return target; }
 	glm::mat4 &getViewMatrix() { return viewMatrix; }
 	glm::mat4 &getProjMatrix() { return projMatrix; }
 
 	float getFov() const { return fov; }
 
 private:
+	void updateView();
 	glm::mat4 viewMatrix;
 	glm::mat4 projMatrix;
 
 	glm::vec3 eye;
-	glm::vec3 target;
+	glm::vec3 rotation;
 	glm::vec3 up;
 
 	float moveSpeed;
 	float rotateSpeed;
-	float dRot;
 
-	float zoomSpeed;
 	float fov;
 	float near;
 	float far;
